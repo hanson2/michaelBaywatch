9169a7d205e772eb500018285030dba46df1dfb6
Keith Hanson <hanson2@rose-hulman.edu>
2017-06-28 16:01:42 -0400

    made framework for future use

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('wookie')
+const app = {
+    init: function(formSelector){
+        document
+            .querySelector(formSelector)
+            .addEventListener("submit", this.handleSubmit)
+        
+    },
+    handleSubmit: function(ev){
+        ev.preventDefault()
+        const f = ev.target
+        console.log(f.flickName.value)
+    },
+}
+
+
+app.init("form#flick-form")
