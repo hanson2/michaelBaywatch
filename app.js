9b9ee421365ec6aac5ae2407f601831f1dab5837
Keith Hanson <hanson2@rose-hulman.edu>
2017-06-28 16:21:27 -0400

    made the methods look neat

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = {
     
-    init: function(selectors){
+    init(selectors){
         this.list = document.querySelector(selectors.listSelector)
         this.max = 0
         
@@ -8,20 +8,19 @@ const app = {
             .querySelector(selectors.formSelector)
             .addEventListener("submit", this.handleSubmit.bind(this))
     },
-    handleSubmit: function(ev){
+    handleSubmit(ev){
         ev.preventDefault()
         const f = ev.target
         const flick = {
             id: this.max+1,
             name: f.flickName.value,
         }
-        console.log(flick)
         this.max++
 
         this.list.appendChild(this.renderListItem(flick))
         
     },
-    renderListItem: function(flick){
+    renderListItem(flick){
         const item = document.createElement('li')
         item.textContent = flick.name
         return item
