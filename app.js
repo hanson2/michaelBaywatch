dd4fe0451305fff1189139baa692ebdae83bd4c3
Keith Hanson <hanson2@rose-hulman.edu>
2017-06-28 21:30:06 -0400

    got update and favorite working in console

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = {
     
     init(selectors){
+        this.items = []
         this.list = document.querySelector(selectors.listSelector)
-        this.max = 0
-        
+        this.max = 0;
         document
             .querySelector(selectors.formSelector)
             .addEventListener("submit", this.handleSubmit.bind(this))
@@ -14,16 +14,53 @@ const app = {
         const flick = {
             id: this.max+1,
             name: f.flickName.value,
+            favorite:false,
+            remove(){
+                
+
+                removeListItem(flick)
+            },
+            fav(){
+                if(this.favorite){
+                    this.favorite=false
+                }else{
+                    this.favorite = true
+                }
+            },
+            shift(up){
+                if(up){
+                    id--
+                }else{
+                    id++
+                }
+            }
+
         }
+        this.items.push(flick)
         this.max++
 
-        this.list.appendChild(this.renderListItem(flick))
+        this.renderListItem(flick)
         
     },
     renderListItem(flick){
         const item = document.createElement('li')
         item.textContent = flick.name
-        return item
+        item.style.backgroundColor = 'white'
+        if(flick.favorite){
+            item.style.backgroundColor = 'black'
+            item.style.color = 'white'
+        }
+        item.setAttribute('id', flick.id)
+        this.list.appendChild(item)
+    },
+    removeListItem(flick){
+        const element = document.getElementById(flick.id)
+        this.list.removeChild(element)
+    },
+    update(){
+        this.items.map(this.removeListItem.bind(this))
+        const temp = this.items.map(this.renderListItem.bind(this))
+        
     },
 }
 
